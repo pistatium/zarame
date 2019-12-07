82f835c07a6a0be58d9f998eae11053d11c88fc3
kimihiro_n <pistatium@users.noreply.github.com>
2019-12-07 13:58:24 +0900

    Bump up to 0.0.2

--- setup.py
+++ setup.py
@@ -6,13 +6,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='zarame',
-    version='0.0.1',
+    version='0.0.2',
     url='https://github.com/pistatium/zarame',
     author='pistatium',
     description='Simple structural transducer using NamedTuple',
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    install_requires=[],
+    install_requires=[
+    ],
     extras_require={
         'test': [
             'pytest'
